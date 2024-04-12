0296e6c64a2788d8fe66f6c486ccc9fd8234ad6a
Thomas NG <hkdjyu@gmail.com>
2024-04-13 00:00:26 +0800

    update test.py

--- test.py
+++ test.py
@@ -20,33 +20,50 @@ class VoiceChatApp:
 
         # input fields for name
 
-        self.name_label = tk.Label(root, text="Username:")
+        self.name_label = tk.Label(root, text="Username: User")
         self.name_label.pack()
 
-        self.name_entry = tk.Entry(root)
-        self.name_entry.pack()
+        self.layout_frame_0 = tk.Frame(root)
+        self.layout_frame_0.pack()
+
+        self.name_entry = tk.Entry(self.layout_frame_0)
+        self.name_entry.pack(side=tk.LEFT)
         self.name_entry.insert(tk.END, "User")
-        self.name_entry.bind("<Return>", lambda event: self.on_name_change(event))
+
+        self.name_entry_button = tk.Button(self.layout_frame_0, text="Set", command=lambda: self.on_name_change())
+        self.name_entry_button.pack(side=tk.RIGHT, padx=5)
+
 
         self.divider2 = tk.Frame(root, height=2, bd=1, relief=tk.SUNKEN)
         self.divider2.pack(fill=tk.X, padx=5, pady=5)
 
         # input fields for IP and port, for creating server
 
-        self.ip_label = tk.Label(root, text="IP Address:")
+        self.ip_label = tk.Label(root, text="IP Address: localhost")
         self.ip_label.pack()
 
-        self.ip_entry = tk.Entry(root)
-        self.ip_entry.pack()
+        self.layout_frame_1 = tk.Frame(root)
+        self.layout_frame_1.pack()
+
+        self.ip_entry = tk.Entry(self.layout_frame_1)
+        self.ip_entry.pack(side=tk.LEFT)
         self.ip_entry.insert(tk.END, "localhost")
 
-        self.port_label = tk.Label(root, text="Port:")
+        self.ip_entry_button = tk.Button(self.layout_frame_1, text="Set", command=lambda: self.on_ip_change())
+        self.ip_entry_button.pack(side=tk.RIGHT, padx=5)
+
+        self.port_label = tk.Label(root, text="Port: 3280")
         self.port_label.pack()
 
-        self.port_entry = tk.Entry(root)
-        self.port_entry.pack()
-        self.port_entry.insert(tk.END, "32800")
-        self.port_entry.bind("<Return>", lambda event: self.on_port_change(event))
+        self.layout_frame_2 = tk.Frame(root)
+        self.layout_frame_2.pack()
+
+        self.port_entry = tk.Entry(self.layout_frame_2)
+        self.port_entry.pack(side=tk.LEFT)
+        self.port_entry.insert(tk.END, "3280")
+
+        self.port_entry_button = tk.Button(self.layout_frame_2, text="Set", command=lambda: self.on_port_change())
+        self.port_entry_button.pack(side=tk.RIGHT, padx=5)
 
         self.start_server_button = tk.Button(root, text="Start Server", command=self.start_server)
         self.start_server_button.pack()
@@ -72,7 +89,7 @@ class VoiceChatApp:
         self.layout_frame = tk.Frame(root)
         self.layout_frame.pack()
 
-        self.refresh_button = tk.Button(self.layout_frame, text="Refresh", command=self.update_chat_rooms)
+        self.refresh_button = tk.Button(self.layout_frame, text="Refresh", command=self.on_refresh_room_click)
         self.refresh_button.pack(side=tk.LEFT)
 
         self.join_chat_room_button = tk.Button(self.layout_frame, text="Join Chat Room", command=self.join_chat_room)
@@ -105,14 +122,25 @@ class VoiceChatApp:
         self.client = None
         self.server_thread = None
         self.client_thread = None
+        self.refresh_room_thread = None
+        self.is_refreshing = False
+        self.available_chat_rooms = []
 
-    def on_name_change(self, event):
+    def on_name_change(self):
         self.username = self.name_entry.get()
+        self.name_label.config(text=f"Username: {self.username}")
 
-    def on_port_change(self, event):
+    def on_ip_change(self):
+        self.ip_address = self.ip_entry.get()
+        self.ip_label.config(text=f"IP Address: {self.ip_address}")
+
+    def on_port_change(self):
         self.port = int(self.port_entry.get())
+        self.port_label.config(text=f"Port: {self.port}")
 
     def on_room_select(self, event):
+        if self.available_chat_rooms.__len__() == 0:
+            return
         self.join_chat_room_button.config(state=tk.NORMAL)
         try:
             self.selected_chat_room = self.chat_room_listbox.get(self.chat_room_listbox.curselection())
@@ -120,19 +148,40 @@ class VoiceChatApp:
         except Exception as e:
             self.selected_chat_room = None
         return
+    
+    def on_refresh_room_click(self):
+        if self.is_refreshing is not True:
+            self.is_refreshing = True
+            self.refresh_button.config(text=" Stop ")
+            self.refresh_room_thread = threading.Thread(target=self.update_chat_rooms)
+            self.refresh_room_thread.start()
+        else:
+            self.is_refreshing = False
+            self.refresh_button.config(text="Refresh")
+        return
 
     def update_chat_rooms(self):
-        # check available chat rooms, port 32800-32809
+        # check available chat rooms, port 3280-3289
         self.chat_room_listbox.delete(0, tk.END)
+        self.status_label.config(text="Updating chat rooms ...")
         for i in range(10):
+            if self.is_refreshing is False:
+                self.status_label.config(text="Chat rooms update stopped.")
+                return
+            self.status_label.config(text="Updating chat rooms ... checking port " + str(3280 + i))
             try:
-                uri = f"ws://{self.ip_entry.get()}:{32800 + i}"
+                uri = f"ws://{self.ip_entry.get()}:{3280 + i}"
                 hostname, port = asyncio.run(self.check_chat_room(uri))
                 if port and hostname:
-                    print(f"Found chat room {i} at port {port}")
+                    print(f"Found chat room {hostname} at port {port}")
                     self.chat_room_listbox.insert(tk.END, f"{hostname}'s Room ({port})")
+                    print(f"Updated chat room listbox")
+                    self.available_chat_rooms.append(f"{hostname}'s Room ({port})")
+            except WindowsError as e:
+                print(f"Unable to connect to {uri}")
             except Exception as e:
-                pass
+                print(e)
+        self.status_label.config(text="Chat rooms updated.")
         return
     
     async def check_chat_room(self, uri):
@@ -149,7 +198,20 @@ class VoiceChatApp:
                 return recv_msg_name, recv_msg_string
         return None, None
 
+    def set_enable_settings(self, enable):
+        self.name_entry.config(state=enable)
+        self.ip_entry.config(state=enable)
+        self.port_entry.config(state=enable)
+        self.name_entry_button.config(state=enable)
+        self.ip_entry_button.config(state=enable)
+        self.port_entry_button.config(state=enable)
+        self.start_server_button.config(state=enable)
+        self.join_server_button.config(state=enable)
+        self.refresh_button.config(state=enable)
+        self.join_chat_room_button.config(state=enable)
+
     def start_server(self):
+        self.set_enable_settings(tk.DISABLED)
         ip = self.ip_entry.get()
         port = int(self.port_entry.get())
         self.server_thread = threading.Thread(target=self.start_server_thread, args=(ip, port))
@@ -193,7 +255,7 @@ class VoiceChatApp:
             if msg_type == "client-info":
                 print(f"Client connected, {msg_username}: {msg_string}")
                 self.chat_listbox.insert(tk.END, f"New client connected: {msg_username}\n")
-                self.chat_listbox.see(tk.END)
+                # self.chat_listbox.see(tk.END)
                 uri = f"ws://{str(self.ip_address)}:{str(self.port)}"
                 print(f"Sending server info to {msg_username}: {uri}")
                 send_msg = self.encode_string_message(str(self.username), "server-info", uri)
@@ -207,7 +269,7 @@ class VoiceChatApp:
                     if msg_username and msg_type and msg_string:
                         if msg_type == "text-message":
                             self.chat_listbox.insert(tk.END, f"{msg_username}: {msg_string}\n")
-                            self.chat_listbox.see(tk.END)
+                            # self.chat_listbox.see(tk.END)
                     await asyncio.sleep(0.1)
         
         # print(f"Client name: {msg_username}\nmsg_type: {msg_type}\nmsg_string: {msg_string}")
@@ -229,6 +291,7 @@ class VoiceChatApp:
         return f"<{username}/><{msg_type}/><{msg_string}/>"
 
     def join_server(self):
+        self.set_enable_settings(tk.DISABLED)
         ip = self.ip_entry.get()
         port = int(self.port_entry.get())
         self.client_thread = threading.Thread(target=self.connect_to_server, args=(ip, port))
@@ -257,7 +320,7 @@ class VoiceChatApp:
                     print(f"Received server info: {recv_msg_string}")
                     # self.client = websocket
                     self.chat_listbox.insert(tk.END, f"Connected to {recv_msg_name}'s Room\n")
-                    self.chat_listbox.see(tk.END)
+                    # self.chat_listbox.see(tk.END)
 
                     # listen for messages
                     while self.is_running:
@@ -267,7 +330,7 @@ class VoiceChatApp:
                         if msg_username and msg_type and msg_string:
                             if msg_type == "text-message":
                                 self.chat_listbox.insert(tk.END, f"{msg_username}: {msg_string}\n")
-                                self.chat_listbox.see(tk.END)
+                                # self.chat_listbox.see(tk.END)
                         await asyncio.sleep(0.1)
 
         except asyncio.CancelledError:
@@ -279,6 +342,7 @@ class VoiceChatApp:
 
     def join_chat_room(self):
         if self.selected_chat_room:
+            self.set_enable_settings(tk.DISABLED)
             chat_room_port = int(self.selected_chat_room.split("(")[1].split(")")[0])
             print(f"Joining chat room {chat_room_port}")
             self.client_thread = threading.Thread(target=self.connect_to_server, args=(self.ip_address, chat_room_port))
@@ -304,11 +368,11 @@ class VoiceChatApp:
         msg_username, msg_type, msg_string = self.decode_string_message(msg)
         if self.client:
             self.chat_listbox.insert(tk.END, f"{self.username}: {msg_string}\n")
-            self.chat_listbox.see(tk.END)
+            # self.chat_listbox.see(tk.END)
             await self.client.send(msg)
         elif self.server:
             self.chat_listbox.insert(tk.END, f"{self.username}: {msg_string}\n")
-            self.chat_listbox.see(tk.END)
+            # self.chat_listbox.see(tk.END)
             await self.server.send(msg)
         return
 
